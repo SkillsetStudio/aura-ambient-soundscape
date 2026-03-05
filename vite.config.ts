4bb6ba36a9d754de9ea9391f885c785371f4583d
Rayhan Uddin Khan <rayhanuddinkhan1990@gmail.com>
2026-03-06 02:17:18 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: "/aura-ambient-soundscape/",
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
@@ -22,6 +23,3 @@ export default defineConfig(({mode}) => {
     },
   };
 });
-export default defineConfig({
-  base: "/aura-ambient-soundscape/",
-})
